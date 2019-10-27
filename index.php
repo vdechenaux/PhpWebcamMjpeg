1cebe41438d9eed753a171d7eef7e81a46658f26
Vincent Dechenaux <vincent.dechenaux@gmail.com>
2019-10-27 23:08:26 +0100

    Add first lines of code

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PHP Webcam demo</title>
+</head>
+<body>
+    <img src="/mjpeg.php" />
+</body>
+</html>
---
+++ mjpeg.php
@@ -0,0 +1,30 @@
+<?php
+
+use VDX\Webcam\Webcam;
+
+require_once 'vendor/autoload.php';
+
+$webcam = new Webcam();
+if (!$webcam->open()) {
+    die('Cannot open webcam');
+}
+
+header('Content-Type: multipart/x-mixed-replace; boundary="VDX_MJPEG_SERVER"');
+
+$filename = tempnam(sys_get_temp_dir(), 'webcam').'.jpg';
+
+for ($i=0; $i<1000; $i++) {
+    if ($webcam->saveFrame($filename)) {
+        echo "--VDX_MJPEG_SERVER\r\nContent-Type: image/jpeg\r\n\r\n";
+        echo file_get_contents($filename);
+        echo "\r\n";
+        ob_flush();
+        flush();
+    }
+}
+
+echo "--VDX_MJPEG_SERVER\r\n";
+
+$webcam->close();
+
+unlink($filename);
